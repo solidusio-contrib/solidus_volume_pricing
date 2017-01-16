ba0f307afd54deac1d8b5dae46cd46f625ac754f
Martin Meyerhoff <martin@stembolt.com>
2017-01-16 17:27:12 +0100

    Remove String monkeypatch `to_range`
    The functionality of this monkeypatch is now contained in
    SolidusVolumePricing::RangeFromString.

--- lib/solidus_volume_pricing/engine.rb
+++ lib/solidus_volume_pricing/engine.rb
@@ -14,26 +14,6 @@ module SolidusVolumePricing
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
-
-      String.class_eval do
-        def to_range
-          case count('.')
-          when 2
-            elements = split('..')
-            return Range.new(elements[0].delete('(').to_i, elements[1].to_i)
-          when 3
-            elements = split('...')
-            return Range.new(elements[0].delete('(').to_i, elements[1].to_i - 1)
-          else
-            raise ArgumentError.new(
-              I18n.t(
-                :'activerecord.errors.messages.could_not_conver_to_range',
-                number: self
-              )
-            )
-          end
-        end
-      end
     end
 
     config.autoload_paths += %W(#{config.root}/lib)
