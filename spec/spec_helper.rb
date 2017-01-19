15473c1492db07be6b848691eec632c57b0c0cdd
Thomas von Deyen <thomas@stembolt.com>
2017-01-19 19:06:50 +0100

    Remove from generators and engine.rb from coverage
    The generators, the engine.rb class and the version file has no code relevant to coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter "/lib/generators"
+  add_filter "/lib/solidus_volume_pricing"
+end
 
 ENV['RAILS_ENV'] ||= 'test'
 
