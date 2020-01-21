a6fb9de05ce09b9941077467e2d54e5dc118cb82
Mattia Roccoberton <mat@blocknot.es>
2020-01-21 16:05:47 +0100

    Upgrade the extension using solidus_dev_support

--- spec/factories/volume_price_factory.rb
+++ spec/factories/volume_price_factory.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :volume_price, class: Spree::VolumePrice do
+  factory :volume_price, class: 'Spree::VolumePrice' do
     amount { 10 }
     discount_type { 'price' }
     range { '(1..5)' }
     association :variant
   end
 
-  factory :volume_price_model, class: Spree::VolumePriceModel do
+  factory :volume_price_model, class: 'Spree::VolumePriceModel' do
     name { 'name' }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
+# Configure Rails Environment
 ENV['RAILS_ENV'] ||= 'test'
 
+# Run Coverage report
 require 'solidus_dev_support/rspec/coverage'
 
 require File.expand_path('dummy/config/environment.rb', __dir__)
 
+# Requires factories and other useful helpers defined in spree_core.
 require 'solidus_dev_support/rspec/feature_helper'
 
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = false
+end
