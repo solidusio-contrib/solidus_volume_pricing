f1140619cad593f8d325cb984f071ae2380df98e
César Carruitero <ccarruitero@protonmail.com>
2021-02-08 19:43:17 -0500

    move factories into lib folder

--- lib/solidus_volume_pricing/factories.rb
+++ lib/solidus_volume_pricing/factories.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  factory :volume_price, class: 'Spree::VolumePrice' do
+    amount { 10 }
+    discount_type { 'price' }
+    range { '(1..5)' }
+    association :variant
+  end
+
+  factory :volume_price_model, class: 'Spree::VolumePriceModel' do
+    name { 'name' }
+  end
 end
