053e3f270df98ea51bc84fa1d0b5b182df8352aa
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-06-07 11:33:42 -0600

    Updated factories loading after recent changes in solidus This commit reflects changes made by Solidus [PR #169] (solidusio/solidus_dev_support#169 (comment)) When testing extensions using Solidus vesions that support the definition_file_paths, it will load the core factory first and then the ones defined in `lib/extension_name/testing_support/factories`. Additionally, this extention makes use of assertions (i.e. `assert_nothing_raised) found in the version of Rails not used by Solidus 2.10, therefore it must be manually configured in `Rspec.config`.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,8 +15,8 @@ require 'solidus_dev_support/rspec/feature_helper'
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
-# Requires factories defined in lib/solidus_volume_pricing/factories.rb
-require 'solidus_volume_pricing/factories'
+# Requires factories defined in lib/solidus_volume_pricing/testing_support/factories.rb
+SolidusDevSupport::TestingSupport::Factories.load_for(SolidusVolumePricing::Engine)
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
