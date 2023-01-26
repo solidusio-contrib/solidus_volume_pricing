8ff81fd13f8ad936829eea33db6bb7a3e7feacc6
Michelle Crisologo <michelle.crisologo@gmail.com>
2023-01-26 16:39:06 +0800

    Bump version to 1.1.0

--- lib/solidus_volume_pricing/version.rb
+++ lib/solidus_volume_pricing/version.rb
@@ -1,20 +1,5 @@
 # frozen_string_literal: true
 
 module SolidusVolumePricing
-  module_function
-
-  # Returns the version of the currently loaded SolidusVolumePricing as a
-  # <tt>Gem::Version</tt>.
-  def version
-    Gem::Version.new VERSION::STRING
-  end
-
-  module VERSION
-    MAJOR = 1
-    MINOR = 0
-    TINY  = 0
-    PRE   = nil
-
-    STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
-  end
+  VERSION = '1.1.0'
 end
